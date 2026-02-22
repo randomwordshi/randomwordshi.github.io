4db67ae9d6c3c4ad0f5c1ffb68566edcfad7cc06
randomwordshi <leo.j.williams@icloud.com>
2026-02-21 22:07:31 -0600

    script.js

--- script.js
+++ script.js
@@ -2,13 +2,8 @@ let count = 0;
 
 const clickBtn = document.getElementById("clickBtn");
 const counter = document.getElementById("counter");
-const themeToggle = document.getElementById("themeToggle");
 
 clickBtn.addEventListener("click", () => {
     count++;
     counter.textContent = "Clicks: " + count;
-});
-
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
 });
